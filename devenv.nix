d7d77bd6f920fa6cddc0de3d7ed911a3513d40a3
Tris Oaten <tris@oat.sh>
2026-05-03 11:33:36 +0100

    add devenv

---
+++ devenv.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+    ];
+  };
+
+  packages = with pkgs; [
+    bacon
+    openssl
+  ];
+
+  scripts.watcher = {
+    exec = ''
+      RUSTFLAGS=-Awarnings watchexec -r --clear=reset -e rs --wrap-process=none "cargo run -q"
+    '';
+    packages = [ pkgs.watchexec ];
+    description = "Rebuilds and runs app with supressed warnings";
+  };
+
+  #
+  # C LIBRARIES
+  #
+  # env.LD_LIBRARY_PATH = lib.makeLibraryPath [
+  #   pkgs.zlib
+  # ];
+}
